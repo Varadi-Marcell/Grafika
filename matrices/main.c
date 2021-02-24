e81c8acb99424a159e0179eec7733e1919306ea0
Varadi-Marcell <78903391+Varadi-Marcell@users.noreply.github.com>
2021-02-24 22:32:36 +0100

    Add files via upload

---
+++ matrices/main.c
@@ -0,0 +1,36 @@
+#include "matrix.h"
+
+int main(int argc, char* argv[])
+{
+	float a[3][3] = {
+        { 1.0f, -2.0f,  3.0f},
+        { 5.0f, -3.0f,  0.0f},
+        {-2.0f,  1.0f, -4.0f}
+    };
+	float b[3][3];
+	float c[3][3];
+
+	float p[3];
+
+    init_zero_matrix(b);
+    b[1][1] =  8.0f;
+    b[2][0] = -3.0f;
+    b[2][2] =  5.0f;
+
+    print_matrix(a);
+    print_matrix(b);
+    
+    add_matrices(a, b, c);
+
+    print_matrix(c);
+
+	init_identity_matrix(a);
+	printf("identity matrix:\n");
+	print_matrix(a);
+	
+	mult_scalar_matrix(b, 10000, a);
+	printf("Multiplay matrix:\n");
+	print_matrix(b);
+	return 0;
+}
+
---
+++ matrices/matrix.h
@@ -0,0 +1,29 @@
+#ifndef MATRIX_H
+#define MATRIX_H
+
+/**
+ * Initializes all elements of the matrix to zero.
+ */
+void init_zero_matrix(float matrix[3][3]);
+
+/**
+ * Initializes an identity matrix.
+ */
+void init_identity_matrix(float matrix[3][3]);
+
+/**
+ * Print the elements of the matrix.
+ */
+void print_matrix(const float matrix[3][3]);
+/**
+* Multply a matrix by a real value.
+*/
+void mult_scalar_matrix(float result[3][3], float scalar,const float matrix[3][3]);
+
+/**
+ * Add matrices.
+ */
+void add_matrices(const float a[3][3], const float b[3][3], float c[3][3]);
+
+#endif // MATRIX_H
+
